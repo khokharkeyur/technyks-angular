8b32d0a66ae1e4e3872e33490e48e5d4452bcf37
khokharkeyur <keyurkhokhar04@gmail.com>
2024-12-16 06:26:12 +0530

    Form validationin admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,51 @@
+// import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    //  NgIf
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
-  model: any;
+  model: any = {};
+  cover!: string | undefined;
+  cover_file: any;
+  showError = false;
 
-  onSumbit(form: NgForm) {}
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.cover_file = file;
+      const reader = new FileReader();
+      console.log(reader);
+      reader.onload = () => {
+        const dataUrl = reader.result?.toString();
+        this.cover = dataUrl;
+      };
+      reader.readAsDataURL(file);
+      this.showError = false;
+    }
+  }
+
+  onSumbit(form: NgForm) {
+    if (form.invalid || !this.cover) {
+      console.log('form invalid');
+      form.control.markAllAsTouched();
+      if (!this.cover) {
+        this.showError = true;
+      }
+      return;
+    }
+    if (this.cover) {
+      console.log('form invalid');
+      form.control.markAllAsTouched();
+      return;
+    }
+    console.log(form.value);
+  }
 }

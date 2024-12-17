da56aa859228cd71a67947e43dd9d98690b9ca5f
khokharkeyur <keyurkhokhar04@gmail.com>
2024-12-17 05:39:24 +0530

    create header and about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'about', component: AboutComponent },
 ];

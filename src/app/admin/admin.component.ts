b12acc2f61e83870f387419a456a5710ca0bdd1e
khokharkeyur <keyurkhokhar04@gmail.com>
2024-12-20 18:33:15 +0530

    refactor admin component to improve course display and data handling

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,7 +1,8 @@
 // import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { CoursesComponent } from "../courses/courses.component";
+import { CoursesComponent } from '../courses/courses.component';
+import { Strings } from '../enum/strings.enum';
 // import { NgFor } from '@angular/common';
 
 @Component({
@@ -10,8 +11,8 @@ import { CoursesComponent } from "../courses/courses.component";
     FormsModule,
     // NgIf,
     // NgFor,
-    CoursesComponent
-],
+    CoursesComponent,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
@@ -20,7 +21,18 @@ export class AdminComponent {
   cover!: string | undefined;
   cover_file: any;
   showError = false;
-  courses:any[] = [];
+  courses: any[] = [];
+
+  ngOnInit() {
+    this.getCourses();
+  }
+  getCourses() {
+    const data = localStorage.getItem(Strings.STORAGE_KEY);
+    console.log('data', data);
+    if (data) {
+      this.courses = JSON.parse(data);
+    }
+  }
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
@@ -46,11 +58,17 @@ export class AdminComponent {
       }
       return;
     }
-    if (this.cover) {
-      console.log('form invalid');
-      form.control.markAllAsTouched();
-      return;
-    }
     console.log(form.value);
+    this.saveCourse(form.value);
+  }
+  saveCourse(formValue: any) {
+    console.log('formValue', formValue);
+    const data = {
+      ...formValue,
+      image: this.cover,
+      id: this.courses.length + 1,
+    }
+    this.courses = [ ...this.courses, data]
+    localStorage.setItem(Strings.STORAGE_KEY, JSON.stringify(this.courses));
   }
 }
